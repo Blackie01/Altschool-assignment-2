2026372b1a4627cd50d62e8e529f848b1fd98880
Blackie01 <damola24795@gmail.com>
2022-10-29 05:21:46 +0100

    committ

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import Card from "./Components/card";
+import PageNotFound from "./Components/error404";
 import { Routes, Route, Link } from "react-router-dom";
 
 // first page
@@ -17,27 +19,61 @@ const Jumbo = () => {
 
 //second page
 function ApiCall() {
-  function ApiCallCode() {
-    fetch("https://www.boredapi.com/api/activity")
-      .then((response) => response.json())
-      .then((data) => setProfile(data.profile));
+  // set state for the profiles
+  const [profile, setProfile] = useState([]);
+
+  // set state for the pages
+  const [page, setPage] = useState(1)
+
+  // reusable API call function
+  const ReusableAPI = () =>{
+    fetch("https://randomuser.me/api/?results=10")
+    .then((response) => response.json())
+    .then((json) => setProfile(json.results));
   }
 
-  const [profile, setProfile] = useState(["Go home"]);
+  //API call with useEffect
+  useEffect(() => {ReusableAPI()}, []);
 
-  useEffect(() => {
-    ApiCallCode();
-  });
+  //function for the pagination
+  const Pagination = (val) => {
+    setPage(val)
+    ReusableAPI()
+  }
 
-  console.log(profile);
+  // checking the functionality of pagination
+  console.log(page)
 
   return (
-    <section>
+    <section className="apipage-container">
+      
       <Link to="/">
-        <button>Back</button>
+        <button className="backButton">Home</button>
       </Link>
 
-      <div>{profile}</div>
+
+      {profile.length === 0 && <h1 className="loader">Loading...</h1>}
+
+      <div className="card-container">
+        {profile.map((user, i) => (
+          <div key={i}>
+          <Card userData={user} />
+          </div>
+        ))}
+      </div>
+
+      <div className="page-flex">
+          <button className="btn" onClick={()=>Pagination(page-1)} disabled={page===1}>prev</button>
+          <button className={`${page===1?'active':'btn'}`} onClick={()=>Pagination(1)}>1</button>
+          <button className={`${page===2?'active':'btn'}`} onClick={()=>Pagination(2)}>2</button>
+          <button className={`${page===3?'active':'btn'}`} onClick={()=>Pagination(3)}>3</button>
+          <button className={`${page===4?'active':'btn'}`} onClick={()=>Pagination(4)}>4</button>
+          <button className={`${page===5?'active':'btn'}`} onClick={()=>Pagination(5)}>5</button>
+          <button className={`${page===6?'active':'btn'}`} onClick={()=>Pagination(6)}>6</button>
+          <button className={`${page===7?'active':'btn'}`} onClick={()=>Pagination(7)}>7</button>
+          <button className={`${page===8?'active':'btn'}`} onClick={()=>Pagination(8)}>8</button>
+          <button className="btn" onClick={()=>Pagination(page+1)} disabled={page===8}>next</button>
+      </div>
     </section>
   );
 }
@@ -48,6 +84,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Jumbo />} />
         <Route path="/apicall" element={<ApiCall />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </section>
   );
@@ -70,3 +107,6 @@ a list format
 
 
 */
+
+//manage state to show loading spinner
+//for the pagination, on click of any pagination, load new 10. 
